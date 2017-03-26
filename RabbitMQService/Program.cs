c415c7b0daa376c9533105c5ec95e322b7c3c076
wangyongmao <wangyongmao@yiguo.com>
2017-03-26 22:43:24 +0800

    添加项目文件。

---
+++ RabbitMQService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RabbitMQService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
