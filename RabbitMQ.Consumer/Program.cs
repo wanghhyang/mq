38e989df89ff837f78f50f963c92474ed05bc92c
wangyongmao <wangyongmao@yiguo.com>
2017-03-28 22:10:05 +0800

    mq试用

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMQ.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+            factory.UserName = "yy";
+            factory.Password = "hello";
+            using(var connection = factory.CreateConnection())
+            {
+                using(var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("hello", false, false, false, null);
+                    var consumer = new QueueingBasicConsumer(channel);
+                    channel.BasicConsume("hello", true, consumer);
+                    Console.WriteLine("waiting for message.");
+                    while (true)
+                    {
+                        var ea = consumer.Queue.Dequeue();
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine("Received {0}", message);
+                    }
+
+                }
+            }
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
--- RabbitMQService/Program.cs
+++ RabbitMQService/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace RabbitMQService
 {
@@ -6,6 +7,22 @@ namespace RabbitMQService
     {
         static void Main(string[] args)
         {
+            //RabbitMQ.Client.
+            RabbitMQ.Client.ConnectionFactory factory = new RabbitMQ.Client.ConnectionFactory();
+            factory.HostName = "localhost";
+            factory.UserName = "yy";
+            factory.Password = "hello";
+            using(var connnection = factory.CreateConnection())
+            {
+                using (var channel = connnection.CreateModel())
+                {
+                    channel.QueueDeclare("hello", false, false, false, null);
+                    string message = "Hello world";
+                    var body = Encoding.UTF8.GetBytes(message);
+                    channel.BasicPublish("", "hello",false, null, body);
+                    Console.WriteLine(" set {0}",message );
+                }
+            }
             Console.WriteLine("Hello World!");
         }
     }
